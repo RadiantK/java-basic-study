1092bfbabb05d9c4a1f6e108c65e995101856320
MinKook Kang <95058915+RadiantK@users.noreply.github.com>
2022-01-01 23:49:24 +0900

    Queue LinkedList 연습

--- javabook_collection/ex11_Queue_ex/QueueStudyTest.java
+++ javabook_collection/ex11_Queue_ex/QueueStudyTest.java
@@ -2,50 +2,52 @@ package ex11_Queue_ex;
 
 import java.util.*;
 
+@SuppressWarnings("rawtypes")
 public class QueueStudyTest {
-
-	@SuppressWarnings("rawtypes")
+		
 	static Queue q = new LinkedList();
 	static final int MAX_SIZE = 5;
 	
 	@SuppressWarnings("unchecked")
-	public static void save(String input) {
-		if(!"".equals(input)) {
+	static void save(String input) {
+		if(input!=null && !input.equals("")) {
 			q.offer(input);
 		} 
-		if (q.size() > MAX_SIZE) {
+		if (q.size()>MAX_SIZE) {
 			q.poll();
 		}
 	}
-	@SuppressWarnings({ "rawtypes", "resource" })
+
 	public static void main(String[] args) {
 
-		System.out.println("help를 입력하면 도움말을 볼 수 있습니다.");
+		System.out.println("help를 입력하면 도움말을 확인할 수 있습니다.");
 		
 		while(true) {
-			System.out.print(">> ");
+			System.out.print(">>");
+			
 			try {
 				Scanner sc = new Scanner(System.in);
-				String input = sc.nextLine().trim();
+				String input = sc.nextLine();
 				
-				if(input.equalsIgnoreCase("help")) {
-					System.out.println(" help - 도움말을 보여줍니다.");
-					System.out.println(" q 또는 Q - 프로그램을 종료합니다.");
-					System.out.println(" history - 최근에 입력한 명령어를 5개 보여줍니다.");
-				} else if ("".equals(input)) {
+				if(input.equals("")) {
 					continue;
 				} else if (input.equalsIgnoreCase("q")) {
-					System.out.println("프로그램 종료");
-					System.exit(MAX_SIZE);
+					System.out.println("프로그램 종료 !");
+					System.exit(0);
+					sc.close();
+				} else if (input.equalsIgnoreCase("help")) {
+					System.out.println("help => 도움말을 보여줍니다.");
+					System.out.println("q 또는 Q => 프로그램을 종료합니다.");
+					System.out.println("history => 최근에 입력한 명령어 "+MAX_SIZE+"개를 불러옵니다.");
 				} else if (input.equalsIgnoreCase("history")) {
 					save(input);
 					
-					LinkedList list = (LinkedList)q;
+					LinkedList ll = (LinkedList)q;
 					
-					final int SIZE = list.size();
-					for(int i=0; i<SIZE; i++) {
-						System.out.println((i+1)+"."+list.get(i));
-					} 
+					final int SIZE = ll.size();
+					for (int i=0; i<SIZE; i++) {
+						System.out.println((i+1)+". "+ll.get(i));
+					}
 				} else {
 					save(input);
 					System.out.println(input);
@@ -53,9 +55,7 @@ public class QueueStudyTest {
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
-						
 		}
 	}
-	
 
 }
