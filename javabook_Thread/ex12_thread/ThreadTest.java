ae0757153df11a7d51fd0fff71ce58dbbeffdb4a
MinKook Kang <95058915+RadiantK@users.noreply.github.com>
2022-01-02 22:46:58 +0900

    쓰레드

---
+++ javabook_Thread/ex12_thread/ThreadTest.java
@@ -0,0 +1,33 @@
+package ex12_thread;
+
+class MyThread1 extends Thread {
+	
+	public void run() {
+		for(int i=0; i<50; i++) {
+			System.out.print(this.getName());
+		}
+	}
+}
+
+class MyThread2 implements Runnable {
+	@Override
+	public void run() {
+		for(int i=0; i<50; i++) {
+			System.out.print(Thread.currentThread().getName());
+		}
+	}
+}
+
+public class ThreadTest {
+	public static void main(String[] args) {
+
+		MyThread1 t1 = new MyThread1();
+		
+		Runnable r = new MyThread2();
+		Thread t2 = new Thread(r);
+		
+		t1.start();
+		t2.start();
+	}
+
+}
---
+++ javabook_Thread/ex12_thread/ThreadTest2.java
@@ -0,0 +1,43 @@
+package ex12_thread;
+
+class Thread12 extends Thread {
+	@Override
+	public void run() {
+		for (int i=0; i<100; i++) {
+			System.out.print(new String("ㅡ"));
+		}
+	}
+}
+
+class Thread22 extends Thread {
+	
+	@Override
+	public void run() {
+		for (int i=0; i<100; i++) {
+			System.out.print(new String("ㅣ"));
+		}
+	}
+}
+
+public class ThreadTest2 {
+
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		
+		Thread12 t12 = new Thread12();
+		Thread22 t22 = new Thread22();
+		
+		t12.start();
+		t22.start();
+		
+		try {
+			t12.join();
+			t22.join();
+		} catch(InterruptedException e) {
+			System.out.println(e);
+		}
+		
+		System.out.print("소요시간: "+ (System.currentTimeMillis()-startTime));
+	}
+
+}
