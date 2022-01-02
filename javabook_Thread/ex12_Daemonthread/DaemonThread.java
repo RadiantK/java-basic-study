bf7c22d30ef03d767062bee39d21dadd97c81ff9
MinKook Kang <95058915+RadiantK@users.noreply.github.com>
2022-01-02 23:52:26 +0900

    데몬 쓰레드

---
+++ javabook_Thread/ex12_Daemonthread/DaemonThread.java
@@ -0,0 +1,42 @@
+package ex12_Daemonthread;
+
+public class DaemonThread implements Runnable {
+	static boolean autoSave = false;
+	
+	public static void main(String[] args) {
+
+		Thread t = new Thread(new DaemonThread());
+		t.setDaemon(true); // 보조쓰레드는 일반쓰레드가 하나도 없으면 자동종료
+		t.start();
+		
+		for(int i=1; i<=10; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			System.out.println(i);
+			
+			if(i==5) {
+				autoSave = true;
+			}
+			
+			System.out.println("프로그램을 종료합니다.");
+		}
+	}
+	public void run() {
+		while(true) { // 무한루프
+			try {
+				Thread.sleep(3000);
+			} catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			if(autoSave) {
+				autoSave();
+			}
+		}
+	}
+	public void autoSave() {
+		System.out.println("작업파일이 자동 저장되었습니다.");
+	}
+}
