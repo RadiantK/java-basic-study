a3dd58a3f611e5ebdcdfaa8d56ba548e06a65426
RadiantK <pps8853@gmail.com>
2022-01-14 10:48:52 +0900

    동기화

--- javabook_Thread/ex13_synchronizedThread/SynchronizedTest.java
+++ javabook_Thread/ex13_synchronizedThread/SynchronizedTest.java
@@ -1,41 +1,48 @@
 package ex13_synchronizedThread;
 
 class Account {
-	private int balance = 1000;
+	private int balance = 1500;
 	
-	public synchronized int getBalance() {
-		return balance;
+	public int getBalance() {
+		return this.balance;
 	}
-	public synchronized void withDraw(int money) {
-		if(balance >= money) {
-			try {
-				Thread.sleep(1000);
-			} catch(InterruptedException e) {
-				
+	
+	public void withdraw(int money) {
+		synchronized(this) {
+			if(balance >= money) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				balance -= money;
 			}
-			balance -=money;
-		}	
+		}
 	}
 }
-class Runnable12 implements Runnable {
+
+class Runnable11 implements Runnable {
 	Account acc = new Account();
 	
 	@Override
 	public void run() {
 		while(acc.getBalance() > 0) {
 			int money = (int)(Math.random()*3+1)*100;
-			acc.withDraw(money);
-			System.out.println("balance: "+acc.getBalance());
+			acc.withdraw(money);
+			System.out.println("balance : "+acc.getBalance());
 		}
 	}
 }
+
 
 public class SynchronizedTest {
 
 	public static void main(String[] args) {
 
-		Runnable12 r = new Runnable12();
-		new Thread(r).start();
+		Runnable11 ra = new Runnable11();
+		new Thread(ra).start();
+		new Thread(ra).start();
+		
 	}
 
 }
